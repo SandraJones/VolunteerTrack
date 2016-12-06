6fa6eace1fef7bb5c40e39e6bc00555fb835d94d
Sandy Jones <sandynashv@gmail.com>
2016-12-05 18:03:39 -0600

    added VolunteerContext

---
+++ VolunteerTrack/DAL/VolunteerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VolunteerTrack.DAL
+{
+    public class VolunteerContext: DbContext
+    {
+        // 'virtual' is for Moq during testing
+        public virtual DbSet<VolunteerUser> Users { get; set; }
+        public virtual DbSet<CharityFocus> Focuses { get; set; } 
+        public virtual DbSet<VolunteerActivity> Activities { get; set; }
+        public virtual DbSet<CharitableOrganization> CharitableOrganizations { get; set; }
+    }
+}
