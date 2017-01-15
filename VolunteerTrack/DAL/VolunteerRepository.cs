09d578e6b1beb7eeda8539758864eaee3688ffae
Sandy Jones <sandynashv@gmail.com>
2017-01-14 20:57:53 -0600

    added comments in repo

--- VolunteerTrack/DAL/VolunteerRepository.cs
+++ VolunteerTrack/DAL/VolunteerRepository.cs
@@ -54,13 +54,32 @@ namespace VolunteerTrack.DAL
                 Context.SaveChanges();
             }
         }
-       
+
+        /*
+        This method edits a volunteer activity from the database. 
+        This is accomplished by the PUT method in the C# 
+        ActivitiesController.
+
+        Arguments:
+        Activity - the activity that the user clicks the edit button for,  
+        while on the MyActivities page, Activities.cshtml.
+        */
+
         public void UpdateActivity(VolunteerActivity _activity)
         {
             Context.Entry(_activity).State = System.Data.Entity.EntityState.Modified;
             Context.SaveChanges();     
         }
-       
+
+
+        /*
+        This method gets the activity by the activityId from the database. 
+        The ActivitiesController GET method that takes an argument is the
+        one used here.
+        
+        Arguments:
+        ActivityId - the id of the activity 
+        */
         public VolunteerActivity GetActivityById(int activity_Id)
         {
             VolunteerActivity found_activity = Context.Activities.FirstOrDefault(i => i.ActivityId == activity_Id);
@@ -72,8 +91,16 @@ namespace VolunteerTrack.DAL
             {
                 return null;
             }
-        } 
-         
+        }
+
+        /*
+       This method gets all activities from the database. 
+       This is accomplished by the GET method in the C# 
+       ActivitiesController that does not take an argument..
+
+       Arguments:
+       None
+       */
         public List<VolunteerActivity> GetAllActivitiesForAllUsers()
         {
             return Context.Activities.ToList();
@@ -84,6 +111,15 @@ namespace VolunteerTrack.DAL
             //this goes thru each activity and checks the username within the baseuser within the volunteeruser table and checks for a match, and adds to list if matched.
             return Context.Activities.Where(activity => activity.VolunteerUser.BaseUser.UserName == UserName).ToList();
         }
+
+        /*
+        This method calculates the total number of hours (the user has volunteered) from the database. 
+        This is accomplished by the GET method in the C# SummaryActivitiesController.
+        
+        Arguments:
+        UserName - the username of the current user 
+        */
+
         public int calculateYTDNumberHours(string UserName)
         {
 
@@ -91,12 +127,29 @@ namespace VolunteerTrack.DAL
             var totalHours = AllActivities.Sum(activity => activity.NumberHours);
             return totalHours;//this gets all hours for all dates
         }
+
+        /*
+        This method calculates the total mileage the user has entered into the database from the input form. 
+        This is accomplished by the GET method in the C# SummaryActivitiesController.
+        
+        Arguments:
+        UserName - the username of the current user 
+        */
+
         public int calculateYTDMileage(string UserName)
         {
             var AllActivities = this.GetAllActivitiesForCurrentUser(UserName);
             var totalMileage = AllActivities.Sum(activity => activity.Mileage); //this gets all mileage for all dates
             return totalMileage;
         }
+
+        /*
+        This method calculates the total dollars contributed by the user. 
+        This is accomplished by the GET method in the C# SummaryActivitiesController.
+        
+        Arguments:
+        UserName - the username of the current user 
+        */
 
         public int calculateYTDDollarsContributed(string UserName)
         {
@@ -105,10 +158,12 @@ namespace VolunteerTrack.DAL
             return totalDollars;
         }
 
+
         public VolunteerUser GetUserByUserName(string UserName)
         {
             return Context.VolunteerUsers.Where(v => v.BaseUser.UserName == UserName).FirstOrDefault();
         }
+
 
         public void CreateVolunteerUser(string UserName)
         {
@@ -118,6 +173,7 @@ namespace VolunteerTrack.DAL
             Context.VolunteerUsers.Add(VolUser);
             Context.SaveChanges();
         }
+
 
         public ApplicationUser GetAppUserByUserName(string UserName)
         {
